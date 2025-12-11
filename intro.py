454bd32d1daef35e81f09faaf2860f7982b6b8bc
Roman Shrestha <romanstha306@gmail.com>
2025-12-11 12:33:57 +0545

    introduction to python

--- intro.py
+++ intro.py
@@ -8,7 +8,7 @@ height = 5.7  # in feet
 print(sys.version) # Print Python version through sys module
 # print(f"My name is {name}, I am {age} years old and my height is {height} feet.")
 
-
+print('')
 print('My name is',name) # print using commas
 print(f'My name is {name}, I am {age} years old and my height is {height} feet.') # print using f strings 
 print("My name is {}, I am {} years old and my height is {} feet.".format(name, age, height)) # print using format method       
@@ -16,3 +16,33 @@ print("My name is %s, I am %d years old and my height is %.1f feet." % (name, ag
 print("My name is " + name + ", I am " + str(age) + " years old and my height is " + str(height) + " feet.") # print using concatenation
 
 # f strings are the most preferred way to format strings in Python 3.6 and above due to their readability and efficiency.
+
+wealth1 = True
+wealth2 = False
+wealth3 = None
+
+# print(f'Is he rich? {rich}')
+print('')
+print(f'Is he wealthy? {wealth1}')
+print(f'Is he poor? {wealth2}')
+print(f'Is his wealth status known? {wealth3}')
+
+print('')
+print(type(name))  #<class 'str'>
+print(type(age))  #<class 'int'>
+print(type(height))  #<class 'float'>
+print(type(wealth1))  #<class 'bool'>
+print(type(wealth3))  #<class 'NoneType'>
+
+
+print('')      
+# Example of f-string
+value = 'roman'
+txt = f'My name is {value}'
+print(txt)
+
+var = value*2
+output = (value + '2')*4  # Multiplying string by integer repeats the string
+
+print(output)
+print(type(output))

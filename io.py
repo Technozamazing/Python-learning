18efb475e9c186e24ce7292c5d15975aaafe42e0
Roman Shrestha <romanstha306@gmail.com>
2025-12-11 13:19:29 +0545

    input in python

---
+++ io.py
@@ -0,0 +1,30 @@
+# How to take input from user and display output in Python
+
+'''
+input() function is used to take input from user in Python.
+By default, input() function takes input as string
+
+# for string input
+name = input("Enter your name: ")
+print(f'Hello, {name}!')
+
+# for integer input
+age = int(input("Enter your age: "))
+print(f'Your age is {age} years old.')
+
+# for float input
+height = float(input("Enter your height in feet: "))
+print(f'Your height is {height} feet.')
+
+'''
+
+import sys
+
+name = input("Enter your name: ")
+print(f'Hello, {name}!')
+
+age = int(input("Enter your age: "))
+print(f'Your age is {age} years old.')
+
+height = float(input("Enter your height in feet: "))
+print(f'Your height is {height} feet.')
